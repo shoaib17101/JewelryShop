2b95caf0a4631377f910658f21a5c272b90adbee
Shoaib <shoaibkhan171015393@gmail.com>
2025-10-03 22:08:42 +0500

    app change

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import {Route,BrowserRouter,Routes} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Home from './Pages/Home'
 import Header from './Components/Header'
 function App(){
   return(
     <>
     <Header/>
-  <BrowserRouter>
   <Routes>
-    <Route index  element={<Home/>}></Route>
+    <HashRouter>
+ <Route index  element={<Home/>}></Route>
     <Route path='/home'  element={<Home/>}></Route>
-
+    </HashRouter>
   </Routes>
-  </BrowserRouter>
 
     </>
   )
